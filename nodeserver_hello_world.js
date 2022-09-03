bed03509ff649619e5e0b1ba4d718cbb0e253ed0
aravindpabbi <aravindpabbi@gmail.com>
2022-09-03 16:55:29 -0400

    Added file for simple hello world code using node server

---
+++ nodeserver_hello_world.js
@@ -0,0 +1,8 @@
+let http = require('http');
+
+http.createServer((req,res) => {
+    res.writeHead(200,{"Content-Type": "text/plain"});
+    res.end("Hello World");
+}).listen(8099);
+
+console.log('Server running at http://127.0.0.1:8099/');
