8619f45e89aab6dffba4fd62f28dbb2c93291b00
aravindpabbi <aravindpabbi@gmail.com>
2022-09-03 17:26:46 -0400

    Added changes for node server file

--- nodeserver_hello_world.js
+++ nodeserver_hello_world.js
@@ -2,7 +2,7 @@ let http = require('http');
 
 http.createServer((req,res) => {
     res.writeHead(200,{"Content-Type": "text/plain"});
-    res.end("Hello World");
+    res.end("Hello World using node server");
 }).listen(8099);
 
 console.log('Server running at http://127.0.0.1:8099/');
